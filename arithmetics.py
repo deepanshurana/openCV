af88eff747bbf1004e6a7397923cf5a381097bb8
Deepanshu Rana <deepanshurana4500@gmail.com>
2019-11-12 11:05:15 +0530

    Arithmetics openCV

--- arithmetics.py
+++ arithmetics.py
@@ -36,10 +36,9 @@ cv2.waitKey(4000)
 cv2.destroyAllWindows()
 
 '''
-to give weightage to one image wrt other, use .
-
-
+To give weightage to one image wrt other, use .addWeighted() method. 
 '''
+
 res = cv2.addWeighted(img1, 0.7, img2, 0.3, 0)
 cv2.imshow('addweight', res)
 cv2.waitKey(5000)
